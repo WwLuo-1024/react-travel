996d9f65d77120abcd4f3701978bd64582ba2dc9
WwLuo-1024 <abrahamwong1408@gmail.com>
2023-02-14 20:34:51 +0000

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
+import {Layout, Typography, Input} from "antd" //title属于Typography子组件
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.App}>
+      <div>
+        <Layout.Header>
+          <img src={logo} alt="logo" className = {styles['App-logo']} />
+          <Typography.Title level={3}>React 旅游网</Typography.Title>
+          <Input.Search placeholder={'请输入旅游目的地、主题、或关键字'} />
+        </Layout.Header>
+      </div>
     </div>
   );
 }

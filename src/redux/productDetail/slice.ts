33631b6f3c9438ec019ebfd7a427ede215e5b19c
WwLuo-1024 <abrahamwong1408@gmail.com>
2023-02-25 19:34:36 +0000

    redux toolkit Update

--- src/redux/productDetail/slice.ts
+++ src/redux/productDetail/slice.ts
@@ -1,4 +1,4 @@
-import { createSlice, PayloadAction, createAsyncThunk  } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 interface ProductDetailState {
@@ -16,18 +16,23 @@ const initialState: ProductDetailState = {
 export const getProductDetail = createAsyncThunk(
     "productDetail/getProductDetail",
     async (touristRouteId: string, thunkAPI) => {
-        // setLoading(true)
-        thunkAPI.dispatch(productDetailSlice.actions.fetchStart())
-        try {
-            const { data } = await axios.get(`http://123.56.149.216:8080/api/touristRoutes/${touristRouteId}`);
-            // setProduct(data)
-            // setLoading(false)
-            thunkAPI.dispatch(productDetailSlice.actions.fetchSuccess(data))
-        } catch (error) {
-            // setError(error instanceof Error? error.message : "error")
-            // setLoading(false)
-            thunkAPI.dispatch(productDetailSlice.actions.fetchFail(error instanceof Error ? error.message : "error"))
-        }
+        const { data } = await axios.get(`http://123.56.149.216:8080/api/touristRoutes/${touristRouteId}`);
+        //方法一 手动控制数据流方法
+        // // setLoading(true)
+        // thunkAPI.dispatch(productDetailSlice.actions.fetchStart())
+        // try {
+        //     const { data } = await axios.get(`http://123.56.149.216:8080/api/touristRoutes/${touristRouteId}`);
+        //     // setProduct(data)
+        //     // setLoading(false)
+        //     thunkAPI.dispatch(productDetailSlice.actions.fetchSuccess(data))
+        // } catch (error) {
+        //     // setError(error instanceof Error? error.message : "error")
+        //     // setLoading(false)
+        //     thunkAPI.dispatch(productDetailSlice.actions.fetchFail(error instanceof Error ? error.message : "error"))
+        // }
+        
+        //方法二 返回Promise pending, fulfilled, rejected 需要在下方利用extraReducer
+        return data; //返回Promise
     }
 )
 
@@ -38,16 +43,19 @@ export const productDetailSlice = createSlice({
     //2. 此时recuder是一个对象而不是一个过程 每个对象对应着一个action 同时也对应着action的处理函数
     //3. 本身createSlice就是面对对象而不是面对过程的 所以不需要再使用switch语句
     reducers: {
-        fetchStart: (state) => {
+
+    },
+    extraReducers:{
+        [getProductDetail.pending.type]: (state) => {
             // return {...state, loading: true}; //通过展开操作符 利用state中的旧数据 创建一个全新的state对象 并且更新loading值 遵守了纯函数和immutable
             state.loading = true; //immer处理方式 减少设计模式
         },
-        fetchSuccess: (state, action) => {
+        [getProductDetail.fulfilled.type]: (state, action) => {
             state.data = action.payload;
             state.loading = false; //请求结束
             state.error = null;
         },
-        fetchFail: (state, action: PayloadAction<string | null>) => {
+        [getProductDetail.rejected.type]: (state, action: PayloadAction<string | null>) => {
             state.loading = false;
             state.error = action.payload;
         }

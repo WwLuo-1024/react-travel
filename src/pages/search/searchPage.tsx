fafd9b17dcedd5babfa36e16d9a99f6f6237e93c
WwLuo-1024 <abrahamwong1408@gmail.com>
2023-02-26 20:12:46 +0000

    Search Page Initial

---
+++ src/pages/search/searchPage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from './search/searchPage.module.css'
+
+export const SearchPage: React.FC = () =>{
+    return(
+        <>
+        
+        </>
+    )
+}

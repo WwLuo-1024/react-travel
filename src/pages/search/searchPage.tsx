4041fc267802e9f5c17ba3f0f0ce662fd2d75a8f
WwLuo-1024 <abrahamwong1408@gmail.com>
2023-02-26 20:24:00 +0000

    Componets Added

--- src/pages/search/searchPage.tsx
+++ src/pages/search/searchPage.tsx
@@ -1,10 +1,18 @@
 import React from "react";
-import styles from './search/searchPage.module.css'
+import styles from './searchPage.module.css'
+import { Header, Footer } from "../../components";
 
 export const SearchPage: React.FC = () =>{
     return(
         <>
-        
+            <Header />
+            <div className={styles["page-content"]}>
+            {/* 分类过滤器 */}
+            <div className={styles["product-list-container"]}></div>
+            {/* 产品列表 */}
+            <div className={styles["product-list-container"]}></div>
+            </div>
+            <Footer />
         </>
     )
 }

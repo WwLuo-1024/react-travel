13c3674fc3a3a8d1245fa94260de803cf7142f91
WwLuo-1024 <abrahamwong1408@gmail.com>
2023-02-27 21:39:50 +0000

    Layout Initial

---
+++ src/layouts/mainLayout/mainLayout.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from '.mainLayout.module.css'
+
+export const MainLayout: React.FC = () =>{
+    return(
+        <div>
+            
+        </div>
+    )
+}

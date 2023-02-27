e4854590f12590ddce6309ede27b232f44f844cf
WwLuo-1024 <abrahamwong1408@gmail.com>
2023-02-27 22:23:21 +0000

    UserLayout Initial

---
+++ src/layouts/userLayout/userLayout.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from './userLayout.module.css'
+
+interface PropsType{
+    children: React.ReactNode
+}
+
+export const UserLayout:React.FC<PropsType> = (children) =>{
+    return(
+        <>
+
+        </>
+    )
+}

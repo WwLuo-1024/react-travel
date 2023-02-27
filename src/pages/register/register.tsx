0e50e8295650eb7b8796b9817c119dfb321baeae
WwLuo-1024 <abrahamwong1408@gmail.com>
2023-02-27 22:30:32 +0000

    UserLayout Added

--- src/pages/register/register.tsx
+++ src/pages/register/register.tsx
@@ -1,8 +1,11 @@
 import React from "react";
-import styles from './register.module.css'
+import { UserLayout } from "../../layouts/userLayout";
 
 export const RegisterPage:React.FC = () =>{
     return(
-        <h1>注册页面</h1>
+        <UserLayout>
+            <h1>注册页面</h1>
+        </UserLayout>
+        
     )
 }
--- src/pages/signin/signin.tsx
+++ src/pages/signin/signin.tsx
@@ -1,8 +1,10 @@
 import React from "react";
-import styles from './signin.module.css'
+import { UserLayout } from "../../layouts/userLayout";
 
-export const SignInPage:React.FC = () =>{
-    return(
-        <h1>登陆页面</h1>
+export const SignInPage: React.FC = () => {
+    return (
+        <UserLayout>
+            <h1>登陆页面</h1>
+        </UserLayout>
     )
 }

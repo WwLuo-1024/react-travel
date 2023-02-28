c2fdf775f4104404275255016c7e78f2147b5fa2
WwLuo-1024 <abrahamwong1408@gmail.com>
2023-02-28 01:04:09 +0000

    SignPage Commit

--- src/pages/signin/signin.tsx
+++ src/pages/signin/signin.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { UserLayout } from "../../layouts/userLayout";
+import { SigninForm } from "./signinForm";
 
 export const SignInPage: React.FC = () => {
     return (
         <UserLayout>
-            <h1>登陆页面</h1>
+            {/* <h1>登陆页面</h1> */}
+            <SigninForm />
         </UserLayout>
     )
 }
--- src/pages/signin/signinForm.tsx
+++ src/pages/signin/signinForm.tsx
@@ -1,10 +1,60 @@
 import React from "react";
-import styles from './signinForm.module.css'
+import { Button, Checkbox, Form, Input } from 'antd';
+
 
 export const SigninForm: React.FC = () =>{
+
+    const onFinish = async (values: any) => {
+        console.log('Success:', values);
+
+        try {
+            console.log("登陆成功")
+        } catch (error) {
+            alert("注册失败")
+        }
+
+    };
+
+    const onFinishFailed = (errorInfo: any) => {
+        console.log('Failed:', errorInfo);
+    };
+
     return(
-        <>
-        
-        </>
+        <Form
+            name="basic"
+            labelCol={{ span: 8 }}
+            wrapperCol={{ span: 16 }}
+            style={{ maxWidth: 400, marginLeft: 'auto', marginRight: 'auto' }}
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+        >
+            <Form.Item
+                label="Username"
+                name="username"
+                rules={[{ required: true, message: 'Please input your username!' }]}
+            >
+                <Input />
+            </Form.Item>
+
+            <Form.Item
+                label="Password"
+                name="password"
+                rules={[{ required: true, message: 'Please input your password!' }]}
+            >
+                <Input.Password />
+            </Form.Item>
+
+            <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
+                <Checkbox>Remember me</Checkbox>
+            </Form.Item>
+
+            <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                <Button type="primary" htmlType="submit">
+                    Submit
+                </Button>
+            </Form.Item>
+        </Form>
     )
 }

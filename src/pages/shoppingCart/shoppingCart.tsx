fb0ac5c43745fa689af360585f8256a6ecae06d5
WwLuo-1024 <abrahamwong1408@gmail.com>
2023-03-04 00:40:28 +0000

    ShoppingCart Update

--- src/pages/shoppingCart/shoppingCart.tsx
+++ src/pages/shoppingCart/shoppingCart.tsx
@@ -1,11 +1,30 @@
 import React from "react";
 import styles from './shoppingCart.module.css'
+import { MainLayout } from "../../layouts/mainLayout";
+import { Row, Col, Affix } from 'antd';
+import { ProductList, PaymentCard } from "../../components";
 
 //私有路由嵌入用户JWT信息 用于判断是否登录 启动私有路由
 export const ShoppingCartPage: React.FC = () =>{
     return(
-        <>
-        
-        </>
+        <MainLayout>
+            <Row>
+                {/* 购物车清单 */}
+                <Col span={16}>
+                    <div className={styles["product-list-container"]}>
+                        {/* <ProductList /> */}
+                    </div>
+                </Col>
+                {/* 支付卡组件 */}
+                <Col span={8}>
+                    {/* 锚点元素包裹 使其保持页面滑动时位置不变 */}
+                    <Affix> 
+                        <div className={styles["payment-card-container"]}>
+                            {/* <PaymentCard /> */}
+                        </div>
+                    </Affix>
+                </Col>    
+            </Row>
+        </MainLayout>
     )
 }

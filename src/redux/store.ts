1893a68c9e4db3e1be216d4558bf3de3fd6fff24
WwLuo-1024 <abrahamwong1408@gmail.com>
2023-03-06 11:51:26 +0000

    redux store update

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -11,6 +11,7 @@ import { productSearchSlice } from "./productSearch/slice";
 import { userSlice } from "./user/slice";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
+import { shoppingCartSlice } from "./shoppingCart/slice";
 
 const persistConfig = {
     key: "root",
@@ -23,7 +24,8 @@ const rootReducer = combineReducers({ //The combination root reducer
     recommendProducts: recommendProductsReducer,
     productDetail: productDetailSlice.reducer,
     productSearch: productSearchSlice.reducer,
-    user: userSlice.reducer
+    user: userSlice.reducer,
+    shoppingCart: shoppingCartSlice.reducer
 })
 
 

220c46b094605f66ce449c607f28bae2ac541a1f
WwLuo-1024 <abrahamwong1408@gmail.com>
2023-03-06 12:46:38 +0000

    ShoppingCart Page Update

--- src/pages/shoppingCart/shoppingCart.tsx
+++ src/pages/shoppingCart/shoppingCart.tsx
@@ -3,28 +3,51 @@ import styles from './shoppingCart.module.css'
 import { MainLayout } from "../../layouts/mainLayout";
 import { Row, Col, Affix } from 'antd';
 import { ProductList, PaymentCard } from "../../components";
+import { useSelector, useAppDispatch } from "../../redux/hooks";
+import { clearShoppingCartItem } from "../../redux/shoppingCart/slice";
 
 //私有路由嵌入用户JWT信息 用于判断是否登录 启动私有路由
-export const ShoppingCartPage: React.FC = () =>{
-    return(
+export const ShoppingCartPage: React.FC = () => {
+
+    const loading = useSelector(s => s.shoppingCart.loading)
+    const shoppingCartItems = useSelector(s => s.shoppingCart.items)
+    const jwt = useSelector(s => s.user.token) as string
+    const dispatch = useAppDispatch()
+
+    console.log(shoppingCartItems)
+
+    return (
         <MainLayout>
             <Row>
                 {/* 购物车清单 */}
                 <Col span={16}>
                     <div className={styles["product-list-container"]}>
-                        {/* <ProductList /> */}
+                        
+                        <ProductList data={shoppingCartItems.map(s => s.touristRoute)} />
                     </div>
                 </Col>
                 {/* 支付卡组件 */}
                 <Col span={8}>
                     {/* 锚点元素包裹 使其保持页面滑动时位置不变 */}
-                    <Affix> 
+                    <Affix>
                         <div className={styles["payment-card-container"]}>
-                            {/* <PaymentCard /> */}
+                            <PaymentCard loading={loading}
+                                originalPrice={shoppingCartItems
+                                    .map(s => s.originalPrice)
+                                    .reduce((a, b) => a + b, 0)}
+                                price={shoppingCartItems.map((s) =>
+                                    s.originalPrice * (s.discountPresent ? s.discountPresent : 1))
+                                    .reduce((a, b) => a + b, 0)}
+                                onCheckout={()=>{
+
+                                }}
+                                onShoppingCartClear={()=>{
+                                    dispatch(clearShoppingCartItem({jwt, itemIds: shoppingCartItems.map(s=>s.id)}))
+                                }} />
                             购物车
                         </div>
                     </Affix>
-                </Col>    
+                </Col>
             </Row>
         </MainLayout>
     )
